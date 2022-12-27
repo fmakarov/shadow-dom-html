5e7561b46ab8aecacad067a18850ab6f0c32f4f1
fedro <fjodor.makaroff@gmail.com>
2022-12-27 03:58:42 +0300

    first init

---
+++ js/shadowDeck.js
@@ -0,0 +1,16 @@
+customElements.define('shower-deck', class extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({
+      mode: "open"
+    })
+    const template = document.createElement('template')
+
+    template.innerHTML = `
+      <link rel="stylesheet" href="css/deck.css">
+      <slot></slot>
+    `
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+  }
+})
